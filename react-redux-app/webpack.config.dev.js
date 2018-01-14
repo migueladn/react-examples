2f166d69f87dec67792f3c9a5b2d3c3f5b04f49e
migueladn <migueladan.devel@gmail.com>
2018-01-14 08:50:36 +0000

    trying to implement react hot loader webpack

--- react-redux-app/webpack.config.dev.js
+++ react-redux-app/webpack.config.dev.js
@@ -1,8 +1,12 @@
 import path from 'path';
+import webpack from 'webpack';
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 export default {
   devtool: 'eval-source-map',
   entry: [
+    'webpack-hot-middleware/client',
     path.join(__dirname, '/client/index.js')
   ],
   output: {
@@ -10,12 +14,22 @@ export default {
     publicPath: '/',
     filename: 'bundle.js'
   },
+  devServer: {
+    hot: true
+  },
+  plugins: [
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+
+    new webpack.NamedModulesPlugin(),
+    new webpack.HotModuleReplacementPlugin()
+  ],
   module: {
     loaders: [
       {
         test: /\.js$/,
         include: path.join(__dirname, 'client'),
-        loaders: ['babel-loader']
+        loaders: ['react-hot-loader/webpack','babel-loader']
       }
     ]
   },
